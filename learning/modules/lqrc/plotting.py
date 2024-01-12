612af681da81273fe1cde43a71d9f0d8cf837a15
Julia Schneider <54411881+jschneider03@users.noreply.github.com>
2024-01-11 22:02:36 -0500

    gradient graphing implemented

--- learning/modules/lqrc/plotting.py
+++ learning/modules/lqrc/plotting.py
@@ -1,29 +1,101 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_predictions_and_gradients():
-    pass
+def plot_predictions_and_gradients(dim, x_actual, y_pred, y_actual, gradients, fn):
+    if dim > 3:
+        print("Dimension greater than 3, cannot graph")
+    elif dim == 3:
+        x_actual = x_actual.detach().cpu().numpy()
+        y_pred = y_pred.detach().cpu().numpy()
+        y_actual = y_actual.detach().cpu().numpy()
+
+        fig = plt.figure(constrained_layout=True)
+        ax = fig.add_subplot(projection="3d")
+        ax.scatter(
+            x_actual[:, 0], x_actual[:, 1], y_pred, marker="o", label="Predicted"
+        )
+        ax.scatter(x_actual[:, 0], x_actual[:, 1], y_actual, marker="^", label="Actual")
+        ax.set_xlim(-15, 15)
+        ax.set_ylim(-15, 15)
+        ax.set_zlim(-15, 250)
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("f(x, y)")
+        ax.legend(loc="upper left")
+        ax.set_title("Pointwise Predictions vs Actual")
+        plt.legend(loc="upper left")
+        plt.savefig(f"{fn}.png")
+    elif dim == 2:
+        x_actual = x_actual.detach().cpu().numpy()
+        y_pred = y_pred.detach().cpu().numpy()
+        y_actual = y_actual.detach().cpu().numpy()
+
+        _, ax = plt.subplots()
+        ax.scatter(x_actual, y_pred, label="Predicted")
+        ax.scatter(x_actual, y_actual, label="Actual", alpha=0.5)
+
+        for g in gradients:
+            line_x = np.linspace(g[0].item() - 0.5, g[0].item() + 0.5, 100)
+            line_y = g[2].item() * (line_x - g[0].item()) + g[1].item()
+            ax.plot(line_x, line_y, color="green", alpha=0.5)
+
+        ax.set_ylim(-10, 250)
+        ax.set_xlim(-15, 15)
+        ax.set_ylabel("y")
+        ax.set_xlabel("x")
+        ax.legend(loc="upper left")
+        ax.set_title("Pointwise Predictions vs Actual")
+        plt.savefig(f"{fn}.png")
+    else:
+        print("Dimension less than 2, cannot graph")
 
 
-def plot_pointwise_predictions(x_actual, y_pred, y_actual, fn):
-    x_actual = x_actual.detach().cpu().numpy()
-    y_pred = y_pred.detach().cpu().numpy()
-    y_actual = y_actual.detach().cpu().numpy()
+def plot_pointwise_predictions(dim, x_actual, y_pred, y_actual, fn):
+    if dim > 3:
+        print("Dimension greater than 3, cannot graph")
+    elif dim == 3:
+        x_actual = x_actual.detach().cpu().numpy()
+        y_pred = y_pred.detach().cpu().numpy()
+        y_actual = y_actual.detach().cpu().numpy()
 
-    fig, ax = plt.subplots()
-    ax.scatter(x_actual, y_pred, label="Predicted")
-    ax.scatter(x_actual, y_actual, label="Actual", alpha=0.5)
-    ax.set_ylim(-10, 250)
-    ax.set_xlim(-15, 15)
-    ax.set_ylabel("y")
-    ax.set_xlabel("x")
-    ax.legend(loc="upper left")
-    ax.set_title("Pointwise Predictions vs Actual")
-    plt.savefig(f"{fn}.png")
+        fig = plt.figure(constrained_layout=True)
+        ax = fig.add_subplot(projection="3d")
+        ax.scatter(
+            x_actual[:, 0], x_actual[:, 1], y_pred, marker="o", label="Predicted"
+        )
+        ax.scatter(x_actual[:, 0], x_actual[:, 1], y_actual, marker="^", label="Actual")
+        ax.set_xlim(-15, 15)
+        ax.set_ylim(-15, 15)
+        ax.set_zlim(-15, 250)
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel("f(x, y)")
+        ax.legend(loc="upper left")
+        ax.set_title("Pointwise Predictions vs Actual")
+        plt.legend(loc="upper left")
+        plt.savefig(f"{fn}.png")
+    elif dim == 2:
+        x_actual = x_actual.detach().cpu().numpy()
+        y_pred = y_pred.detach().cpu().numpy()
+        y_actual = y_actual.detach().cpu().numpy()
+
+        _, ax = plt.subplots()
+        ax.scatter(x_actual, y_pred, label="Predicted")
+        ax.scatter(x_actual, y_actual, label="Actual", alpha=0.5)
+        ax.set_ylim(-10, 250)
+        ax.set_xlim(-15, 15)
+        ax.set_ylabel("y")
+        ax.set_xlabel("x")
+        ax.legend(loc="upper left")
+        ax.set_title("Pointwise Predictions vs Actual")
+        plt.savefig(f"{fn}.png")
+    else:
+        print("Dimension less than 2, cannot graph")
 
 
 def plot_loss(loss_arr, fn):
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
     ax.plot(loss_arr)
     ax.set_ylim(min(0, min(loss_arr) - 1.0), max(loss_arr) + 1.0)
     ax.set_xlim(0, len(loss_arr))
--- learning/modules/lqrc/train_benchmark.py
+++ learning/modules/lqrc/train_benchmark.py
@@ -7,12 +7,15 @@ from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 from custom_nn import BaselineMLP, LQRCDataset, QuadraticNetCholesky, CustomCholeskyLoss
 from learning import LEGGED_GYM_LQRC_DIR
-from plotting import plot_loss, plot_pointwise_predictions
+from plotting import (
+    plot_loss,
+    plot_predictions_and_gradients,
+)
 
 DEVICE = "cuda"
 
 
-def generate_nD_quadratic(n, lb, ub, steps, rand_scaling=10.0, A=None, noise=False):
+def generate_nD_quadratic(n, lb, ub, steps, rand_scaling=100.0, A=None, noise=False):
     """
     Generate nD quadratic in the form of x.T @ A @ x
     """
@@ -51,22 +54,18 @@ def model_switch(input_dim):
         ), torch.nn.MSELoss(reduction="mean")
 
 
-# def grad_output_wrt_input(input, output):
-#     torch.autograd.grad(outputs=output, inputs=input, retain_graph=True)
-
-
 if __name__ == "__main__":
     save_model = False
     num_epochs = 2000
-    input_dim = 2
+    input_dim = 1
 
-    X, y = generate_nD_quadratic(input_dim, -10.0, 10.0, steps=1000, noise=False)
+    X, y = generate_nD_quadratic(input_dim, -10.0, 10.0, steps=100, noise=False)
     data = LQRCDataset(X, y)
     training_data, testing_data = random_split(
         data, data.get_train_test_split_len(0.6, 0.4)
     )
     train_dataloader = DataLoader(training_data, batch_size=100, shuffle=True)
-    test_dataloader = DataLoader(testing_data, batch_size=100, shuffle=True)
+    test_dataloader = DataLoader(testing_data, batch_size=1, shuffle=True)
 
     model, loss_fn = model_switch(input_dim)
     print(model)
@@ -98,14 +97,17 @@ if __name__ == "__main__":
     print(f"Finished training at epoch {epoch}, latest loss {training_losses[-1]}")
 
     # test
-    model.eval()
+    # model.eval()  # removed to enable access to the gradients for graphing
     all_inputs = []
     all_predictions = []
     all_targets = []
+    all_gradients = []
     # with torch.no_grad():
     loss_per_batch = []
     for X_batch, y_batch in test_dataloader:
+        # give X_batch a grad_fn
         X_batch.requires_grad_()
+        X_batch = X_batch + 0
         if isinstance(model, QuadraticNetCholesky):
             A_pred = model(X_batch)
             loss = loss_fn(A_pred, X_batch, y_batch)
@@ -119,11 +121,18 @@ if __name__ == "__main__":
         else:
             y_pred = model(X_batch)
             loss = loss_fn(y_pred, y_batch)
+        all_gradients.append(
+            (
+                X_batch.view(-1),
+                y_pred.view(-1),
+                torch.autograd.grad(y_pred, X_batch)[0].view(-1),
+            )
+        )
         loss_per_batch.append(loss.item())
         all_inputs.append(X_batch)
         all_predictions.append(y_pred)
         all_targets.append(y_batch)
-    print("Loss on test set", torch.mean(torch.tensor(loss_per_batch)).item)
+    print("Loss on test set", torch.mean(torch.tensor(loss_per_batch)).item())
 
     time_str = time.strftime("%Y%m%d_%H%M%S")
     save_str = "1D_quadratic"
@@ -135,10 +144,14 @@ if __name__ == "__main__":
             model.state_dict(),
             save_path + f"/model_{time_str}" + ".pt",
         )
-    plot_pointwise_predictions(
+
+    plot_predictions_and_gradients(
+        input_dim + 1,
         torch.vstack(all_inputs),
         torch.vstack(all_predictions),
         torch.vstack(all_targets),
-        f"{save_path}/{time_str}_graph",
+        all_gradients,
+        f"{save_path}/{time_str}_grad_graph",
     )
+
     plot_loss(training_losses, f"{save_path}/{time_str}_loss")
