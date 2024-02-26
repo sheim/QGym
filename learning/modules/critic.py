some cleanup and fixes on normalization

learning/modules/critic.py
@@ -27,9 +27,6 @@ class Critic(nn.Module):
 
     def evaluate(self, critic_observations):
         if self._normalize_obs:
-            critic_observations = self.normalize(critic_observations)
+            with torch.no_grad():
+                critic_observations = self.obs_rms(critic_observations)
         return self.NN(critic_observations).squeeze()
-
-    def normalize(self, observation):
-        with torch.no_grad():
-            return self.obs_rms(observation)

learning/modules/utils/normalize.py
@@ -3,10 +3,6 @@ import torch.nn as nn
 
 
 class RunningMeanStd(nn.Module):
-    """
-    Keeps a running mean to normalize tensor of choice.
-    """
-
     def __init__(self, num_items, epsilon=1e-05):
         super(RunningMeanStd, self).__init__()
         self.num_items = num_items
@@ -46,18 +42,18 @@ class RunningMeanStd(nn.Module):
         if self.training:
             mean = input.mean(tuple(range(input.dim() - 1)))
             var = input.var(tuple(range(input.dim() - 1)))
-        (
-            self.running_mean,
-            self.running_var,
-            self.count,
-        ) = self._update_mean_var_from_moments(
-            self.running_mean,
-            self.running_var,
-            self.count,
-            mean,
-            var,
-            input.size()[0],
-        )
+            (
+                self.running_mean,
+                self.running_var,
+                self.count,
+            ) = self._update_mean_var_from_moments(
+                self.running_mean,
+                self.running_var,
+                self.count,
+                mean,
+                var,
+                input.size()[0],
+            )
 
         current_mean = self.running_mean
         current_var = self.running_var