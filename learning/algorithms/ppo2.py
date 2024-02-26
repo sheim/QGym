fix normalization, make it work for (n, m, var_dim) shaped data

learning/algorithms/ppo2.py
@@ -87,9 +87,7 @@ class PPO2:
         batch_size = total_data // self.num_mini_batches
         generator = create_uniform_generator(data, batch_size, self.num_learning_epochs)
         for batch in generator:
-            with torch.inference_mode():
-                value_batch = self.critic.evaluate(batch["critic_obs"])
-
+            value_batch = self.critic.evaluate(batch["critic_obs"])
             if self.use_clipped_value_loss:
                 target_value_batch = batch["values"]
                 value_clipped = target_value_batch + (

learning/modules/critic.py
@@ -31,6 +31,5 @@ class Critic(nn.Module):
         return self.NN(critic_observations).squeeze()
 
     def normalize(self, observation):
-        # with torch.no_grad():
-        with torch.inference_mode():
+        with torch.no_grad():
             return self.obs_rms(observation)

learning/modules/utils/normalize.py
@@ -7,10 +7,9 @@ class RunningMeanStd(nn.Module):
     Keeps a running mean to normalize tensor of choice.
     """
 
-    def __init__(self, num_items, axis=0, epsilon=1e-05):
+    def __init__(self, num_items, epsilon=1e-05):
         super(RunningMeanStd, self).__init__()
         self.num_items = num_items
-        self.axis = axis
         self.epsilon = epsilon
 
         self.register_buffer(
@@ -45,8 +44,8 @@ class RunningMeanStd(nn.Module):
 
     def forward(self, input):
         if self.training:
-            mean = input.mean(self.axis)
-            var = input.var(self.axis)
+            mean = input.mean(tuple(range(input.dim() - 1)))
+            var = input.var(tuple(range(input.dim() - 1)))
         (
             self.running_mean,
             self.running_var,