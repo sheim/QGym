ae041d472aa43ac9382e06d6df49318b85042e4a
Steve Heim <heim.steve@gmail.com>
2024-03-06 15:22:29 -0500

    WIP: integration of optuna and wandb is problematic. But seems like we should be able to simply use OPTUNA as the back-end for wandb sweeps: https://docs.wandb.ai/guides/launch/sweeps-on-launch

--- scripts/optune.py
+++ scripts/optune.py
@@ -1,4 +1,7 @@
 import optuna
+from optuna.integration.wandb import WeightsAndBiasesCallback
+
+
 from gym.envs import __init__  # noqa: F401
 from gym.utils import get_args, task_registry, randomize_episode_counters
 from gym.utils.logging_and_saving import wandb_singleton
@@ -6,18 +9,18 @@ from gym.utils.logging_and_saving import local_code_save_helper
 from torch.multiprocessing import Process, Queue
 
 
-# Adjusted setup function to accept hyperparameters
+def update_hyperparams(env_cfg, train_cfg, hyperparams):
+    for key, val in hyperparams["env_cfg"].items():
+        setattr(env_cfg, key, val)
+    for key, val in hyperparams["train_cfg"].items():
+        setattr(train_cfg, key, val)
+
+
 def setup(hyperparams):
     args = get_args()
     wandb_helper = wandb_singleton.WandbSingleton()
-
-    # Load configurations
     env_cfg, train_cfg = task_registry.create_cfgs(args)
-
-    # Adjust configurations based on hyperparameters
-    # Example adjustments:
-    # env_cfg.some_parameter = hyperparams["env_some_parameter"]
-    train_cfg.learning_rate = hyperparams["learning_rate"]
+    update_hyperparams(env_cfg, train_cfg, hyperparams)
 
     task_registry.make_gym_and_sim()
     wandb_helper.setup_wandb(env_cfg=env_cfg, train_cfg=train_cfg, args=args)
@@ -25,7 +28,6 @@ def setup(hyperparams):
     randomize_episode_counters(env)
 
     policy_runner = task_registry.make_alg_runner(env, train_cfg)
-
     local_code_save_helper.save_local_files_to_logs(train_cfg.log_dir)
 
     return train_cfg, policy_runner
@@ -41,10 +43,12 @@ def train(train_cfg, policy_runner):
 # Define the objective function for Optuna
 def objective(trial):
     # Hyperparameter suggestions
-    hyperparams = {
-        # "env_some_parameter": trial.suggest_float("env_some_parameter", 0.0, 1.0),
-        "learning_rate": trial.suggest_loguniform("learning_rate", 1e-5, 1e-1),
-    }
+    env_cfg = {}
+    train_cfg = {}
+    train_cfg["learning_rate"] = trial.suggest_loguniform("learning_rate", 1e-5, 1e-1)
+    env_cfg["episode_length_s"] = trial.suggest_uniform("episode_length_s", 1.0, 10.0)
+
+    hyperparams = {"env_cfg": env_cfg, "train_cfg": train_cfg}
 
     queue = Queue()
     p = Process(target=run_setup_and_train, args=(queue, hyperparams))
@@ -64,5 +68,9 @@ def run_setup_and_train(queue, hyperparams):
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="minimize")
-    study.optimize(objective, n_trials=3)
+    # to integrate with our exisitng wandb stuff, we would need to initialize
+    # the wandb_helper out here.
+    wandb_kwargs = {"project": "my-project"}
+    wandbc = WeightsAndBiasesCallback(wandb_kwargs=wandb_kwargs)
+    study.optimize(objective, n_trials=10, callbacks=[wandbc])
     print("Best value: {} (params: {})\n".format(study.best_value, study.best_params))
