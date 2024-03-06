c0dccccf79e83e0131596b11c91ff69ec13e8684
Steve Heim <heim.steve@gmail.com>
2024-03-06 16:08:28 -0500

    WIP: works with wandb disabled, simplified

--- scripts/optune.py
+++ scripts/optune.py
@@ -16,18 +16,14 @@ def update_hyperparams(env_cfg, train_cfg, hyperparams):
         setattr(train_cfg, key, val)
 
 
-def setup(hyperparams):
+def setup(env_cfg, train_cfg):
     args = get_args()
-    wandb_helper = wandb_singleton.WandbSingleton()
-    env_cfg, train_cfg = task_registry.create_cfgs(args)
-    update_hyperparams(env_cfg, train_cfg, hyperparams)
-
     task_registry.make_gym_and_sim()
-    wandb_helper.setup_wandb(env_cfg=env_cfg, train_cfg=train_cfg, args=args)
     env = task_registry.make_env(name=args.task, env_cfg=env_cfg)
     randomize_episode_counters(env)
 
     policy_runner = task_registry.make_alg_runner(env, train_cfg)
+    task_registry.set_log_dir_name(train_cfg)
     local_code_save_helper.save_local_files_to_logs(train_cfg.log_dir)
 
     return train_cfg, policy_runner
@@ -41,17 +37,23 @@ def train(train_cfg, policy_runner):
 
 
 # Define the objective function for Optuna
-def objective(trial):
+def objective(trial, env_cfg, train_cfg):
     # Hyperparameter suggestions
-    env_cfg = {}
-    train_cfg = {}
-    train_cfg["learning_rate"] = trial.suggest_loguniform("learning_rate", 1e-5, 1e-1)
-    env_cfg["episode_length_s"] = trial.suggest_uniform("episode_length_s", 1.0, 10.0)
-
-    hyperparams = {"env_cfg": env_cfg, "train_cfg": train_cfg}
+    # env_hypers = {}
+    # train_hypers = {}
+    # train_hypers["learning_rate"] = trial.suggest_loguniform(
+    #     "learning_rate", 1e-5, 1e-1
+    # )
+    # env_hypers["episode_length_s"] = trial.suggest_uniform(
+    #     "episode_length_s", 1.0, 10.0
+    # )
+    # hyperparams = {"env_cfg": env_hypers, "train_cfg": train_hypers}
+    train_cfg.algorithm.learning_rate = trial.suggest_loguniform(
+        "learning_rate", 1e-5, 1e-1
+    )
 
     queue = Queue()
-    p = Process(target=run_setup_and_train, args=(queue, hyperparams))
+    p = Process(target=run_setup_and_train, args=(env_cfg, train_cfg, queue))
     p.start()
     p.join()
     result = queue.get()
@@ -60,17 +62,45 @@ def objective(trial):
 
 
 # The function to run in a separate process
-def run_setup_and_train(queue, hyperparams):
-    train_cfg, policy_runner = setup(hyperparams)  # Pass hyperparams to setup
+def run_setup_and_train(env_cfg, train_cfg, queue):
+    train_cfg, policy_runner = setup(env_cfg, train_cfg)
     train_result = train(train_cfg, policy_runner)  # Execute training
     queue.put(train_result)
 
 
 if __name__ == "__main__":
+    args = get_args()
+    env_cfg, train_cfg = task_registry.create_cfgs(args)
+    wandb_helper = wandb_singleton.WandbSingleton()
+    wandb_helper.set_wandb_values(args)
+    wandb_helper.setup_wandb(env_cfg=env_cfg, train_cfg=train_cfg, args=args)
+    # if wandb_helper.is_wandb_enabled():
+    #     if sweep_id is None:
+    #         sweep_id = wandb.sweep(
+    #             sweep_config,
+    #             entity=wandb_helper.get_entity_name(),
+    #             project=wandb_helper.get_project_name(),
+    #         )
+    #     wandb.agent(
+    #         sweep_id,
+    #         sweep_wandb_mp,
+    #         entity=wandb_helper.get_entity_name(),
+    #         project=wandb_helper.get_project_name(),
+    #         count=sweep_config["run_cap"],
+    #     )
+    # else:
+    #     print("ERROR: No WandB project and entity provided for sweeping")
+
+    ###########################
+
     study = optuna.create_study(direction="minimize")
     # to integrate with our exisitng wandb stuff, we would need to initialize
     # the wandb_helper out here.
     wandb_kwargs = {"project": "my-project"}
     wandbc = WeightsAndBiasesCallback(wandb_kwargs=wandb_kwargs)
-    study.optimize(objective, n_trials=10, callbacks=[wandbc])
+    study.optimize(
+        lambda trial: objective(trial, env_cfg, train_cfg),
+        n_trials=3,
+        callbacks=[wandbc],
+    )
     print("Best value: {} (params: {})\n".format(study.best_value, study.best_params))
