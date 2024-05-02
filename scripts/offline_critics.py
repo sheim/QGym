9603a7b902896c2fa3e0f9a797897f76d1687203
Steve Heim <heim.steve@gmail.com>
2024-05-02 18:13:41 -0400

    WIP: offline_critics runs

--- scripts/offline_critics.py
+++ scripts/offline_critics.py
@@ -1,8 +1,4 @@
-from gym.envs import __init__  # noqa: F401
-from gym.utils import get_args, task_registry
-from learning.modules import Actor, Critic
 from learning.modules.lqrc import Cholesky
-from gym.utils.helpers import class_to_dict
 from learning.utils import (
     compute_generalized_advantages,
 )
@@ -11,34 +7,39 @@ from gym import LEGGED_GYM_ROOT_DIR
 import os
 import torch
 
+DEVICE = "cuda:0"
 # handle some bookkeeping
-args = get_args()
-env_cfg, train_cfg = task_registry.create_cfgs(args)
-env_cfg = class_to_dict(env_cfg)
-train_cfg = class_to_dict(train_cfg)
 #
-run_name = "May01_10-47-07_lam09standard_critic"
+run_name = "May02_08-59-46_standard_critic"
 log_dir = os.path.join(
-    LEGGED_GYM_ROOT_DIR, "logs", train_cfg["runner"]["experiment_name"], run_name
+    LEGGED_GYM_ROOT_DIR, "logs", "pendulum_standard_critic", run_name
 )
 
 # create critic based on saved policy
-iteration = 1
-loaded_dict = torch.load(os.path.join(log_dir, "model_{}.pt".format(iteration)))
-trained_actor = Actor(num_obs=2, num_actions=1, **train_cfg["actor"])
-trained_actor.load_state_dict(loaded_dict["actor_state_dict"])
-trained_critic = Critic(num_obs=2, **train_cfg["critic"])
-trained_critic.load_state_dict(loaded_dict["critic_state_dict"])
+# iteration = 1
+# loaded_dict = torch.load(os.path.join(log_dir, "model_{}.pt".format(iteration)))
+# trained_actor = Actor(num_obs=2, num_actions=1, **train_cfg["actor"])
+# trained_actor.load_state_dict(loaded_dict["actor_state_dict"])
+# trained_critic = Critic(num_obs=2, **train_cfg["critic"])
+# trained_critic.load_state_dict(loaded_dict["critic_state_dict"])
 
 # create fresh critic
 
-test_critic = Cholesky(num_obs=2, **train_cfg["critic"])
+test_critic_params = {
+    "num_obs": 2,
+    "hidden_dims": None,
+    "activation": "elu",
+    "normalize_obs": False,
+    "output_size": 1,
+    "device": DEVICE,
+}
+test_critic = Cholesky(**test_critic_params).to(DEVICE)
 
 # load data
-
-data = torch.load(os.path.join(log_dir, "data_{}.pt".format(iteration)))
-gamma = train_cfg["algorithm"]["gamma"]
-lam = train_cfg["algorithm"]["lam"]
+iteration = 1
+data = torch.load(os.path.join(log_dir, "data_{}.pt".format(iteration))).to(DEVICE)
+gamma = 0.99
+lam = 0.99
 
 # compute ground-truth
 
