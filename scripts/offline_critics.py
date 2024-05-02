037cc1c630b979152b01f2caf6524e0e134ec7d9
Steve Heim <heim.steve@gmail.com>
2024-05-02 18:27:08 -0400

    trains new critic

--- scripts/offline_critics.py
+++ scripts/offline_critics.py
@@ -1,11 +1,13 @@
 from learning.modules.lqrc import Cholesky
 from learning.utils import (
     compute_generalized_advantages,
+    compute_MC_returns,
+    create_uniform_generator,
 )
-
 from gym import LEGGED_GYM_ROOT_DIR
 import os
 import torch
+from torch import nn
 
 DEVICE = "cuda:0"
 # handle some bookkeeping
@@ -14,14 +16,6 @@ run_name = "May02_08-59-46_standard_critic"
 log_dir = os.path.join(
     LEGGED_GYM_ROOT_DIR, "logs", "pendulum_standard_critic", run_name
 )
-
-# create critic based on saved policy
-# iteration = 1
-# loaded_dict = torch.load(os.path.join(log_dir, "model_{}.pt".format(iteration)))
-# trained_actor = Actor(num_obs=2, num_actions=1, **train_cfg["actor"])
-# trained_actor.load_state_dict(loaded_dict["actor_state_dict"])
-# trained_critic = Critic(num_obs=2, **train_cfg["critic"])
-# trained_critic.load_state_dict(loaded_dict["critic_state_dict"])
 
 # create fresh critic
 
@@ -33,8 +27,9 @@ test_critic_params = {
     "output_size": 1,
     "device": DEVICE,
 }
+learning_rate = 1.0e-4
 test_critic = Cholesky(**test_critic_params).to(DEVICE)
-
+critic_optimizer = torch.optim.Adam(test_critic.parameters(), lr=learning_rate)
 # load data
 iteration = 1
 data = torch.load(os.path.join(log_dir, "data_{}.pt".format(iteration))).to(DEVICE)
@@ -43,14 +38,33 @@ lam = 0.99
 
 # compute ground-truth
 
+episode_rollouts = compute_MC_returns(data, gamma)
+
 # train new critic
 
 data["values"] = test_critic.evaluate(data["critic_obs"])
 data["advantages"] = compute_generalized_advantages(data, gamma, lam, test_critic)
 data["returns"] = data["advantages"] + data["values"]
 
-# plug in supervised learning here, based off of PPO2.update
+mean_value_loss = 0
+counter = 0
+max_gradient_steps = 24
+max_grad_norm = 1.0
+batch_size = 2**16
+generator = create_uniform_generator(
+    data,
+    batch_size,
+    max_gradient_steps=max_gradient_steps,
+)
+for batch in generator:
+    value_loss = test_critic.loss_fn(batch["critic_obs"], batch["returns"])
+    critic_optimizer.zero_grad()
+    value_loss.backward()
+    nn.utils.clip_grad_norm_(test_critic.parameters(), max_grad_norm)
+    critic_optimizer.step()
+    mean_value_loss += value_loss.item()
+    print("Value loss: ", value_loss.item())
+    counter += 1
+mean_value_loss /= counter
 
 # compare new and old critics
-
-end = True
