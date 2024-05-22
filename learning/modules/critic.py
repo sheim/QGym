Fix bugs in generator, so we can set gradient steps explicitly also some tweaks and tuning

learning/modules/critic.py
@@ -30,5 +30,5 @@ class Critic(nn.Module):
     def evaluate(self, critic_observations):
         return self.forward(critic_observations)
 
-    def loss_fn(self, obs, target):
+    def loss_fn(self, obs, target, **kwargs):
         return nn.functional.mse_loss(self.forward(obs), target, reduction="mean")

learning/modules/lqrc/QRCritics.py
@@ -488,7 +488,7 @@ class NN_wLinearLatent(nn.Module):
     def __init__(self, **kwargs):
         super().__init__()
         critic_name = kwargs["critic_name"]
-        device = kwargs["device"]
+        device = kwargs["device"]  # noqa
         self.critic = eval(f"{critic_name}(**kwargs).to(device)")
 
     def forward(self, x, return_all=False):

learning/utils/dict_utils.py
@@ -61,9 +61,7 @@ def compute_generalized_advantages(data, gamma, lam, critic):
 
 # todo change num_epochs to num_batches
 @torch.no_grad
-def create_uniform_generator(
-    data, batch_size, num_epochs=1, max_gradient_steps=None, keys=None
-):
+def create_uniform_generator(data, batch_size, max_gradient_steps=100):
     n, m = data.shape
     total_data = n * m
 
@@ -71,12 +69,19 @@ def create_uniform_generator(
         batch_size = total_data
 
     num_batches_per_epoch = total_data // batch_size
-    if max_gradient_steps:
-        num_epochs = max_gradient_steps // num_batches_per_epoch
-        num_epochs = max(num_epochs, 1)
-    for epoch in range(num_epochs):
+    num_epochs = max_gradient_steps // num_batches_per_epoch
+    last_epoch_batches = max_gradient_steps - num_epochs * num_batches_per_epoch
+
+    for epoch in range(num_epochs - 1):
         indices = torch.randperm(total_data, device=data.device)
         for i in range(num_batches_per_epoch):
+            batched_data = data.flatten(0, 1)[
+                indices[i * batch_size : (i + 1) * batch_size]
+            ]
+            yield batched_data
+    else:
+        indices = torch.randperm(total_data, device=data.device)
+        for i in range(last_epoch_batches):
             batched_data = data.flatten(0, 1)[
                 indices[i * batch_size : (i + 1) * batch_size]
             ]

scripts/multiple_offline_critics.py
@@ -15,7 +15,9 @@ from torch import nn  # noqa F401
 
 DEVICE = "cuda:0"
 # handle some bookkeeping
-run_name = "May15_16-20-21_standard_critic"  # "May13_10-52-30_standard_critic"  # "May13_10-52-30_standard_critic"
+run_name = (
+    "May22_11-11-03_standard_critic"
+)  # "May13_10-52-30_standard_critic"  # "May13_10-52-30_standard_critic"
 log_dir = os.path.join(
     LEGGED_GYM_ROOT_DIR, "logs", "pendulum_standard_critic", run_name
 )
@@ -48,7 +50,7 @@ critic_params = {
         "normalize_obs": False,
         "minimize": False,
         "latent_dim": 16,
-        "latent_hidden_dims": [4, 8],
+        "latent_hidden_dims": [64, 32],
         "latent_activation": ["elu", "elu"],
         "device": DEVICE,
     },
@@ -59,7 +61,7 @@ critic_params = {
         "normalize_obs": False,
         "minimize": False,
         "latent_dim": 16,
-        "latent_hidden_dims": [4, 8],
+        "latent_hidden_dims": [64, 32],
         "latent_activation": ["elu", "elu"],
         "device": DEVICE,
     },
@@ -69,9 +71,9 @@ critic_params = {
         "activation": ["elu", "elu", "elu"],
         "normalize_obs": False,
         "minimize": False,
-        "relative_dim": 4,
-        "latent_dim": 16,
-        "latent_hidden_dims": [4, 8],
+        "relative_dim": 2,
+        "latent_dim": 5,
+        "latent_hidden_dims": [64, 32],
         "latent_activation": ["elu", "elu"],
         "device": DEVICE,
     },
@@ -116,19 +118,19 @@ critic_params = {
         "device": DEVICE,
     },
     "NN_wQR": {
-        "critic_name": "CholeskyInput",
+        "critic_name": "SpectralLatent",
         "action_dim": 2,
     },
 }
 
 learning_rate = 0.001
 critic_names = [
-    # "Critic",
-    "CholeskyInput",
+    "Critic",
+    # "CholeskyInput",
     # "CholeskyLatent",
     # "PDCholeskyInput",
     # "PDCholeskyLatent",
-    # "SpectralLatent",
+    "SpectralLatent",
     # ]
     # "Cholesky",
     # "CholeskyPlusConst",
@@ -151,11 +153,11 @@ critic_optimizers = {
     name: torch.optim.Adam(critic.parameters(), lr=learning_rate)
     for name, critic in test_critics.items()
 }
-gamma = 0.99
+gamma = 0.95
 lam = 0.95
 tot_iter = 200
 
-for iteration in range(1, tot_iter, 1):
+for iteration in range(50, tot_iter, 10):
     # load data
     base_data = torch.load(os.path.join(log_dir, "data_{}.pt".format(iteration))).to(
         DEVICE
@@ -171,33 +173,31 @@ for iteration in range(1, tot_iter, 1):
     graphing_data["values"]["Ground Truth MC Returns"] = episode_rollouts[0, :]
     graphing_data["returns"]["Ground Truth MC Returns"] = episode_rollouts[0, :]
 
-    for name, test_critic in test_critics.items():
+    for name, critic in test_critics.items():
         critic_optimizer = critic_optimizers[name]
         data = base_data.detach().clone()
         # train new critic
-        data["values"] = test_critic.evaluate(data["critic_obs"])
-        data["advantages"] = compute_generalized_advantages(
-            data, gamma, lam, test_critic
-        )
+        data["values"] = critic.evaluate(data["critic_obs"])
+        data["advantages"] = compute_generalized_advantages(data, gamma, lam, critic)
         data["returns"] = data["advantages"] + data["values"]
 
         mean_value_loss = 0
         counter = 0
         max_gradient_steps = 100
         # max_grad_norm = 1.0
-        batch_size = 10 * 4096
+        batch_size = 64
         generator = create_uniform_generator(
             data,
             batch_size,
             max_gradient_steps=max_gradient_steps,
         )
         for batch in generator:
-            value_loss = test_critic.loss_fn(
+            value_loss = critic.loss_fn(
                 batch["critic_obs"], batch["returns"], actions=batch["actions"]
             )
             critic_optimizer.zero_grad()
             value_loss.backward()
-            # # noqa F401.utils.clip_grad_norm_(test_critic.parameters(), max_grad_norm)
+            # # noqa F401.utils.clip_grad_norm_(critic.parameters(), max_grad_norm)
             critic_optimizer.step()
             mean_value_loss += value_loss.item()
             counter += 1
@@ -207,7 +207,7 @@ for iteration in range(1, tot_iter, 1):
         graphing_data["critic_obs"][name] = data[0, :]["critic_obs"]
         graphing_data["values"][name] = data[0, :][
             "values"
-        ]  # test_critic.evaluate(data[0, :]["critic_obs"])
+        ]  # critic.evaluate(data[0, :]["critic_obs"])
         graphing_data["returns"][name] = data[0, :]["returns"]
 
     # compare new and old critics

scripts/offline_critics.py
@@ -15,7 +15,9 @@ import torch
 
 DEVICE = "cuda:0"
 # handle some bookkeeping
-run_name = "May15_16-20-21_standard_critic"  # "May13_10-52-30_standard_critic"  # "May13_10-52-30_standard_critic"
+run_name = (
+    "May22_10-54-14_standard_critic"
+)  # "May13_10-52-30_standard_critic"  # "May13_10-52-30_standard_critic"
 log_dir = os.path.join(
     LEGGED_GYM_ROOT_DIR, "logs", "pendulum_standard_critic", run_name
 )
@@ -56,9 +58,9 @@ for iteration in range(1, tot_iter, 10):
 
     mean_value_loss = 0
     counter = 0
-    max_gradient_steps = 1000
+    max_gradient_steps = 10
     # max_grad_norm = 1.0
-    batch_size = 1000
+    batch_size = 64
 
     with torch.no_grad():
         test_critic.value_offset.copy_(3.295910835894283)
@@ -69,7 +71,7 @@ for iteration in range(1, tot_iter, 10):
 
     # first train the latent representation for a bit
     lat_generator = create_uniform_generator(
-        data, batch_size=1000, max_gradient_steps=100
+        data, batch_size=256, max_gradient_steps=100
     )
 
     latent_optimizer = torch.optim.Adam(test_critic.latent_NN.parameters(), lr=0.001)