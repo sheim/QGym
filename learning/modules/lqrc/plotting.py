262351b24a1baaa18f39029cea930fe3ba57f8a0
Steve Heim <heim.steve@gmail.com>
2024-06-10 16:27:40 -0400

    adjust multi-critic plots to use imshow

--- learning/modules/lqrc/plotting.py
+++ learning/modules/lqrc/plotting.py
@@ -15,7 +15,6 @@ font = {"size": 12}
 matplotlib.rc("font", **font)
 
 
-
 def create_custom_bwr_colormap():
     # Define the colors for each segment
     dark_blue = [0, 0, 0.5, 1]
@@ -38,6 +37,7 @@ def create_custom_bwr_colormap():
     custom_bwr = ListedColormap(colors, name="custom_bwr")
 
     return custom_bwr
+
 
 def create_custom_pink_green_colormap():
     # Define the colors for each segment
@@ -62,20 +62,34 @@ def create_custom_pink_green_colormap():
 
     return custom_pink_green
 
+
 def plot_dim_sweep(x, y, mean_error, max_error, fn, step=5):
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(14, 5))
-    np_x = x #.detach().cpu().numpy()
-    np_y = y #.detach().cpu().numpy()
-    np_mean_error = mean_error #.detach().cpu().numpy()
-    np_max_error = max_error #.detach().cpu().numpy()
+    np_x = x  # .detach().cpu().numpy()
+    np_y = y  # .detach().cpu().numpy()
+    np_mean_error = mean_error  # .detach().cpu().numpy()
+    np_max_error = max_error  # .detach().cpu().numpy()
 
     extent = [np_x.min() - 0.5, np_x.max() + 0.5, np_y.min() - 0.5, np_y.max() + 0.5]
 
     # mean_err = axes[0].pcolormesh(np_x, np_y, np_mean_error, cmap="YlOrRd", vmin=0.0)
-    mean_err = axes[0].imshow(np_mean_error, cmap='YlOrRd', interpolation="nearest", vmin=0, origin="lower", extent=extent)
+    mean_err = axes[0].imshow(
+        np_mean_error,
+        cmap="YlOrRd",
+        interpolation="nearest",
+        vmin=0,
+        origin="lower",
+        extent=extent,
+    )
     # max_err = axes[1].pcolormesh(np_x, np_y, np_max_error, cmap="PuBuGn", vmin=0.0)
-    max_err = axes[1].imshow(np_max_error, cmap='PuBuGn', interpolation='nearest', vmin=0, origin="lower", extent=extent)
-
+    max_err = axes[1].imshow(
+        np_max_error,
+        cmap="PuBuGn",
+        interpolation="nearest",
+        vmin=0,
+        origin="lower",
+        extent=extent,
+    )
 
     x_ticks = np.arange(np_x.min(), np_x.max(), step)
     y_ticks = np.arange(np_y.min(), np_y.max(), step)
@@ -100,20 +114,54 @@ def plot_dim_sweep(x, y, mean_error, max_error, fn, step=5):
     plt.savefig(fn, bbox_inches="tight", dpi=300)
     print(f"Saved to {fn}")
 
-def plot_dim_sweep_mean_std(x, y, avg_mean_error, avg_max_error, std_mean_error, std_max_error, fn, trial_num, step=5):
+
+def plot_dim_sweep_mean_std(
+    x,
+    y,
+    avg_mean_error,
+    avg_max_error,
+    std_mean_error,
+    std_max_error,
+    fn,
+    trial_num,
+    step=5,
+):
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(25, 15))
 
     extent = [x.min() - 0.5, x.max() + 0.5, y.min() - 0.5, y.max() + 0.5]
 
-    # avg_mean_err = axes[0, 0].imshow(avg_mean_error, cmap='YlOrRd', interpolation="nearest", vmin=0, origin="lower", extent=extent)
-    # avg_max_err = axes[0, 1].imshow(avg_max_error, cmap='PuBuGn', interpolation='nearest', vmin=0, origin="lower", extent=extent)
-    # std_mean_err = axes[1, 0].imshow(std_mean_error, cmap='YlOrRd', interpolation="nearest", vmin=0, origin="lower", extent=extent)
-    # std_max_err = axes[1, 1].imshow(std_max_error, cmap='PuBuGn', interpolation='nearest', vmin=0, origin="lower", extent=extent)
-    avg_mean_err = axes[0, 0].imshow(avg_mean_error, cmap='YlOrRd', interpolation="nearest", norm=mcolors.LogNorm(), origin="lower", extent=extent)
-    avg_max_err = axes[0, 1].imshow(avg_max_error, cmap='PuBuGn', interpolation='nearest', norm=mcolors.LogNorm(), origin="lower", extent=extent)
-    std_mean_err = axes[1, 0].imshow(std_mean_error, cmap='YlOrRd', interpolation="nearest", norm=mcolors.LogNorm(), origin="lower", extent=extent)
-    std_max_err = axes[1, 1].imshow(std_max_error, cmap='PuBuGn', interpolation='nearest', norm=mcolors.LogNorm(), origin="lower", extent=extent)
-
+    avg_mean_err = axes[0, 0].imshow(
+        avg_mean_error,
+        cmap="YlOrRd",
+        interpolation="nearest",
+        norm=mcolors.LogNorm(),
+        origin="lower",
+        extent=extent,
+    )
+    avg_max_err = axes[0, 1].imshow(
+        avg_max_error,
+        cmap="PuBuGn",
+        interpolation="nearest",
+        norm=mcolors.LogNorm(),
+        origin="lower",
+        extent=extent,
+    )
+    std_mean_err = axes[1, 0].imshow(
+        std_mean_error,
+        cmap="YlOrRd",
+        interpolation="nearest",
+        norm=mcolors.LogNorm(),
+        origin="lower",
+        extent=extent,
+    )
+    std_max_err = axes[1, 1].imshow(
+        std_max_error,
+        cmap="PuBuGn",
+        interpolation="nearest",
+        norm=mcolors.LogNorm(),
+        origin="lower",
+        extent=extent,
+    )
 
     x_ticks = np.arange(0, x.max(), step)
     y_ticks = np.arange(0, y.max(), step)
@@ -127,12 +175,37 @@ def plot_dim_sweep_mean_std(x, y, avg_mean_error, avg_max_error, std_mean_error,
     axes[1, 0].set_title(f"Mean Error (Standard Deviation Over {trial_num} Trials)")
     axes[1, 1].set_title(f"Max Error (Standard Deviation Over {trial_num} Trials)")
 
-    fig.colorbar(avg_mean_err, ax=axes[0, 0], shrink=0.95, pad=0.1, label=f"Mean Error (Average Over {trial_num} Trials)")
-    fig.colorbar(avg_max_err, ax=axes[0, 1], shrink=0.95, pad=0.1, label=f"Max Error (Average Over {trial_num} Trials)")
-    fig.colorbar(std_mean_err, ax=axes[1, 0], shrink=0.95, pad=0.1, label=f"Mean Error (Standard Deviation Over {trial_num} Trials)")
-    fig.colorbar(std_max_err, ax=axes[1, 1], shrink=0.95, pad=0.1, label=f"Max Error (Standard Deviation Over {trial_num} Trials)")
+    fig.colorbar(
+        avg_mean_err,
+        ax=axes[0, 0],
+        shrink=0.95,
+        pad=0.1,
+        label=f"Mean Error (Average Over {trial_num} Trials)",
+    )
+    fig.colorbar(
+        avg_max_err,
+        ax=axes[0, 1],
+        shrink=0.95,
+        pad=0.1,
+        label=f"Max Error (Average Over {trial_num} Trials)",
+    )
+    fig.colorbar(
+        std_mean_err,
+        ax=axes[1, 0],
+        shrink=0.95,
+        pad=0.1,
+        label=f"Mean Error (Standard Deviation Over {trial_num} Trials)",
+    )
+    fig.colorbar(
+        std_max_err,
+        ax=axes[1, 1],
+        shrink=0.95,
+        pad=0.1,
+        label=f"Max Error (Standard Deviation Over {trial_num} Trials)",
+    )
     plt.savefig(fn, bbox_inches="tight", dpi=300)
     print(f"Saved to {fn}")
+
 
 def plot_pendulum_multiple_critics(
     x, predictions, targets, title, fn, colorbar_label="f(x)"
@@ -228,7 +301,6 @@ def plot_pendulum_multiple_critics_w_data(
 ):
     num_critics = len(x.keys())
     fig, axes = plt.subplots(nrows=2, ncols=num_critics, figsize=(6 * num_critics, 10))
-
     # Determine global min and max error for consistent scaling
     global_min_error = float("inf")
     global_max_error = float("-inf")
@@ -245,17 +317,11 @@ def plot_pendulum_multiple_critics_w_data(
         np_error = np_predictions - np_targets
         global_min_error = min(global_min_error, np.min(np_error))
         global_max_error = max(global_max_error, np.max(np_error))
-        # global_min_prediction = min(global_min_prediction, np.min(np_predictions))
-        # global_max_prediction = max(global_max_prediction, np.max(np_predictions))
         global_min_prediction = np.min(np_targets)
         global_max_prediction = np.max(np_targets)
     error_norm = mcolors.TwoSlopeNorm(
         vmin=global_min_error, vcenter=0, vmax=global_max_error
     )
-    # error_norm = mcolors.TwoSlopeNorm(vmin=-2.50, vcenter=0, vmax=2.50)
-    # prediction_norm = mcolors.BoundaryNorm(
-    #     [global_min_prediction, global_max_prediction], 256
-    # )
     prediction_norm = mcolors.CenteredNorm(
         vcenter=(global_max_prediction + global_min_prediction) / 2,
         halfrange=(global_max_prediction - global_min_prediction) / 2,
@@ -264,40 +330,84 @@ def plot_pendulum_multiple_critics_w_data(
     #     vcenter=0.0,
     #     halfrange=2.5,
     # )
+    grid_size = 64
+    xcord = np.linspace(-2 * np.pi, 2 * np.pi, grid_size)
+    ycord = np.linspace(-5, 5, grid_size)
+
     for ix, critic_name in enumerate(x):
         np_x = x[critic_name].detach().cpu().numpy().reshape(-1, 2)
+        # rescale (hack), value shardcoded from pendulum_config
+        np_x[:, 0] = np_x[:, 0] * 2 * np.pi
+        np_x[:, 1] = np_x[:, 1] * 5
         np_predictions = predictions[critic_name].detach().cpu().numpy().reshape(-1)
         np_targets = (
             targets["Ground Truth MC Returns"].detach().cpu().numpy().reshape(-1)
         )
         np_error = np_predictions - np_targets
 
+        # todo: clean up this super hacky code
+
         # predictions
         # if np.any(~np.equal(np_predictions, np.zeros_like(np_predictions))):
-        axes[0, ix].scatter(
-            np_x[:, 0],
-            np_x[:, 1],
-            c=np_predictions,
+        # axes[0, ix].scatter(
+        #     np_x[:, 0] * 2 * np.pi,
+        #     np_x[:, 1] * 5,
+        #     c=np_predictions,
+        #     cmap=prediction_cmap,
+        #     norm=prediction_norm,
+        #     alpha=0.5,
+        #     # norm=CenteredNorm(),
+        # )
+        axes[0, ix].imshow(
+            np_predictions.reshape(grid_size, grid_size).T,
+            origin="lower",
+            extent=(xcord.min(), xcord.max(), ycord.min(), ycord.max()),
             cmap=prediction_cmap,
             norm=prediction_norm,
-            alpha=0.5,
-            # norm=CenteredNorm(),
+            # shading="auto",
         )
         axes[0, ix].set_title(f"{critic_name} Prediction")
 
-        # error
-        # if np.any(~np.equal(np_error, np.zeros_like(np_error))):
         if ix == 0:
             continue
-        axes[1, ix].scatter(
-            np_x[:, 0],
-            np_x[:, 1],
-            c=np_error,
+        # axes[1, ix].scatter(
+        #     np_x[:, 0],
+        #     np_x[:, 1],
+        #     c=np_error,
+        #     cmap=error_cmap,
+        #     norm=error_norm,
+        #     alpha=0.5,
+        # )
+        axes[1, ix].imshow(
+            np_error.reshape(64, 64).T,
+            origin="lower",
+            extent=(xcord.min(), xcord.max(), ycord.min(), ycord.max()),
             cmap=error_cmap,
             norm=error_norm,
-            alpha=0.5,
+            # shading="auto",
         )
         axes[1, ix].set_title(f"{critic_name} Error")
+
+    data = data.detach().cpu().numpy()
+    theta = data[:, :, 0] * 2 * np.pi
+    omega = data[:, :, 1] * 5
+    # _, _, _, hist = axes[1, 0].hist2d(
+    #     theta.flatten(), omega.flatten(), bins=64, cmap="Blues"
+    # )
+    axes[1, 0].plot(theta, omega, lw=1)
+
+    axes[1, 0].set_xlabel("theta")
+    axes[1, 0].set_ylabel("theta_dot")
+    fig.suptitle(title, fontsize=16)
+
+    # Ensure the axes are the same for all plots
+    for ax in axes.flat:
+        ax.set_xlim([xcord.min(), xcord.max()])
+        ax.set_ylim([ycord.min(), ycord.max()])
+
+    plt.subplots_adjust(
+        top=0.9, bottom=0.1, left=0.1, right=0.9, hspace=0.4, wspace=0.3
+    )
 
     fig.colorbar(
         mpl.cm.ScalarMappable(norm=prediction_norm, cmap=prediction_cmap),
@@ -312,23 +422,20 @@ def plot_pendulum_multiple_critics_w_data(
         label=colorbar_label,
     )
 
-    data = data.detach().cpu().numpy()
-    theta = data[:, :, 0]
-    omega = data[:, :, 1]
-    _, _, _, hist = axes[1, 0].hist2d(
-        theta.flatten(), omega.flatten(), bins=64, cmap="Blues"
-    )
-    axes[1, 0].plot(theta, omega, lw=1)
-
-    axes[1, 0].set_xlabel("theta")
-    axes[1, 0].set_ylabel("theta_dot")
-    fig.suptitle(title, fontsize=16)
     plt.savefig(f"{fn}.png")
     print(f"Saved to {fn}.png")
 
 
 def plot_pendulum_multiple_critics_w_data_grad(
-    x, predictions, targets, title, fn, data, pred_grad, analytic_grad, colorbar_label="f(x)"
+    x,
+    predictions,
+    targets,
+    title,
+    fn,
+    data,
+    pred_grad,
+    analytic_grad,
+    colorbar_label="f(x)",
 ):
     num_critics = len(x.keys())
     fig, axes = plt.subplots(nrows=3, ncols=num_critics, figsize=(6 * num_critics, 10))
@@ -355,8 +462,8 @@ def plot_pendulum_multiple_critics_w_data_grad(
         global_max_error = max(global_max_error, np.max(np_error))
         global_min_prediction = np.min(np_targets)
         global_max_prediction = np.max(np_targets)
-        global_min_grad_error = min(global_min_grad_error, np.min(np_grad_error))
-        global_max_grad_error = max(global_max_grad_error, np.max(np_grad_error))
+        global_min_grad_error = min(global_min_grad_error, np.min(np_grad_error))  # noqa
+        global_max_grad_error = max(global_max_grad_error, np.max(np_grad_error))  # noqa
     error_norm = mcolors.TwoSlopeNorm(
         vmin=global_min_error, vcenter=0, vmax=global_max_error
     )
@@ -403,7 +510,7 @@ def plot_pendulum_multiple_critics_w_data_grad(
         )
         axes[1, ix].set_title(f"{critic_name} Error")
 
-        #pred error
+        # pred error
         axes[2, ix].scatter(
             np_x[:, 0],
             np_x[:, 1],
