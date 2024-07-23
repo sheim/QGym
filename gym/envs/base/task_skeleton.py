bugfix scale action noise, added scaling and refactorings

gym/envs/base/task_skeleton.py
@@ -45,6 +45,7 @@ class TaskSkeleton:
         """Reset all robots"""
         self._reset_idx(torch.arange(self.num_envs, device=self.device))
         self.step()
+        self.episode_length_buf[:] = 0
 
     def _reset_buffers(self):
         self.to_be_reset[:] = False
@@ -67,7 +68,7 @@ class TaskSkeleton:
     def _check_terminations_and_timeouts(self):
         """Check if environments need to be reset"""
         contact_forces = self.contact_forces[:, self.termination_contact_indices, :]
-        self.terminated = torch.any(torch.norm(contact_forces, dim=-1) > 1.0, dim=1)
+        self.terminated |= torch.any(torch.norm(contact_forces, dim=-1) > 1.0, dim=1)
         self.timed_out = self.episode_length_buf >= self.max_episode_length
         self.to_be_reset = self.timed_out | self.terminated
 

gym/envs/mini_cheetah/minimalist_cheetah.py
@@ -22,6 +22,8 @@ class MinimalistCheetah:
 
         # Scales
         self.command_scales = torch.tensor([3.0, 1.0, 3.0]).to(self.device)
+        self.dof_pos_scales = torch.tensor([0.2, 0.3, 0.3]).to(self.device).repeat(4)
+        self.dof_vel_scales = torch.tensor([2.0, 2.0, 4.0]).to(self.device).repeat(4)
 
         # Previous 2 dof pos targets
         self.dof_target_prev = None
@@ -49,15 +51,20 @@ class MinimalistCheetah:
             torch.tensor(commands, device=self.device).unsqueeze(0)
             * self.command_scales
         )
-        self.dof_pos_obs = torch.tensor(dof_pos_obs, device=self.device).unsqueeze(0)
-        self.dof_vel = torch.tensor(dof_vel, device=self.device).unsqueeze(0)
+        self.dof_pos_obs = (
+            torch.tensor(dof_pos_obs, device=self.device).unsqueeze(0)
+            * self.dof_pos_scales
+        )
+        self.dof_vel = (
+            torch.tensor(dof_vel, device=self.device).unsqueeze(0) * self.dof_vel_scales
+        )
         self.grf = torch.tensor(grf, device=self.device).unsqueeze(0)
 
         # Compute phase sin
         phase_obs = torch.tensor(phase_obs, device=self.device).unsqueeze(0)
         self.phase_sin = phase_obs[:, 0].unsqueeze(0)
 
-        # Set targets
+        # Set targets, these are scaled in Robot-Software
         self.dof_pos_target = torch.tensor(
             dof_pos_target, device=self.device
         ).unsqueeze(0)